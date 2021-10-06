111d33ce21b42cba124705fdbed4cc7ce8a680e0
Michał Kotfica <kotvitz17@gmail.com>
2021-10-06 20:47:38 +0200

    added background music in introduction activity

--- app/src/main/java/com/example/hbh/fastestfingerfirst/IntroductionActivity.kt
+++ app/src/main/java/com/example/hbh/fastestfingerfirst/IntroductionActivity.kt
@@ -1,14 +1,19 @@
 package com.example.hbh.fastestfingerfirst
 
+import android.media.MediaPlayer
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 class IntroductionActivity : AppCompatActivity() {
 
+    private lateinit var mediaPlayer: MediaPlayer
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_introduction)
+        mediaPlayer = MediaPlayer.create(applicationContext, R.raw.explain_fastest_finger)
+        mediaPlayer.start()
     }
 
     override fun onWindowFocusChanged(hasFocus: Boolean) {
