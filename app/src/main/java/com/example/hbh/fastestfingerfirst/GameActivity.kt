5a1725a1ad346e632b00cd8a5facbb6631cdb9b0
Michał Kotfica <kotvitz17@gmail.com>
2021-10-06 21:22:18 +0200

    going to game activity + little corrections

---
+++ app/src/main/java/com/example/hbh/fastestfingerfirst/GameActivity.kt
@@ -0,0 +1,14 @@
+package com.example.hbh.fastestfingerfirst
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+
+class GameActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game)
+    }
+}
--- app/src/main/java/com/example/hbh/fastestfingerfirst/IntroductionActivity.kt
+++ app/src/main/java/com/example/hbh/fastestfingerfirst/IntroductionActivity.kt
@@ -1,19 +1,34 @@
 package com.example.hbh.fastestfingerfirst
 
+import android.content.Intent
 import android.media.MediaPlayer
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import kotlin.system.exitProcess
 
 class IntroductionActivity : AppCompatActivity() {
 
-    private lateinit var mediaPlayer: MediaPlayer
+    private lateinit var backgroundMusic: MediaPlayer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_introduction)
-        mediaPlayer = MediaPlayer.create(applicationContext, R.raw.explain_fastest_finger)
-        mediaPlayer.start()
+        backgroundMusic = MediaPlayer.create(applicationContext, R.raw.explain_fastest_finger)
+        backgroundMusic.start()
+        val startGameButton = findViewById<Button>(R.id.startGameButton)
+        startGameButton.setOnClickListener {
+            MediaPlayer.create(applicationContext, R.raw.click).start()
+            backgroundMusic.stop()
+            startActivity(Intent(this, GameActivity::class.java))
+        }
+        val quitButton = findViewById<Button>(R.id.exitButton)
+        quitButton.setOnClickListener {
+            MediaPlayer.create(applicationContext, R.raw.click).start()
+            backgroundMusic.stop()
+            exitProcess(0)
+        }
     }
 
     override fun onWindowFocusChanged(hasFocus: Boolean) {
